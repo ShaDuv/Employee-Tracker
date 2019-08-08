a4aadacd0ab49dec9bd26464dd60a62a41de1747
Daniel Hongel and Shawna Duvall <shawna@>
2019-08-08 10:48:05 -0700

    add faker gem and code to seeds file to populate database.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+Division.destroy_all
+Employee.destroy_all
+# found this solution on stack overflow: https://stackoverflow.com/questions/31478855/seeding-associations-in-rails/31480226#31480226
+20.times do
+  name = Faker::Games::Witcher.location
+  division = Division.create!(name: name)
+  5.times do
+    first_name =  Faker::Name.first_name
+    last_name =  Faker::Name.last_name
+    employee = Employee.new(first_name: first_name, last_name: last_name)
+    employee.division = division
+    employee.save!
+  end
+end
+
+p "Created #{Division.count} divisions"
+p "Created #{Employee.count} employees"
+
+# 20.times do |index|
+#   Employee.create!(
+#     first_name: Faker::Name.first_name,
+#     last_name: Faker::Name.last_name
+#   )
 #
-# Examples:
+# end
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# p "Created #{Employee.count} employees"
